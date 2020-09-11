71b277b90431e2162e4648e31f193139f4a447ee
manish <manishpamnani169@gmail.com>
2020-09-11 12:36:26 +0530

    Adding Second Form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -21,8 +23,10 @@ const appRoutes:Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    ToastrModule.forRoot(),
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,82 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+  checked;
+  gender;
 
-  constructor() { }
+  constructor(private toastrService: ToastrService) { }
 
   ngOnInit() {
+    this.createForm();
+  }
+
+  createForm() {
+    this.firstFormGroup = new FormGroup({
+      fname : new FormControl('', Validators.required),
+      lname : new FormControl('', Validators.required),
+      date : new FormControl('', Validators.required),
+      country : new FormControl('', Validators.required),
+      age : new FormControl('', Validators.required),
+      gender: new FormControl('', Validators.required),
+      terms: new FormControl('', Validators.required)
+    })
+
+    this.secondFormGroup = new FormGroup({
+      satisfaction : new FormControl('', Validators.required),
+    })
+  }
+
+  onChange(event) {
+    if (event.target.checked == false) {
+      let element = document.getElementById('terms_and_conditions');
+      element.style.border = 'solid red 1px'
+      this.firstFormGroup.controls.terms.patchValue(false)
+    } else {
+      this.firstFormGroup.controls.terms.patchValue(true)
+    }
+  }
+
+  getGender(event) {
+    console.log(event.target.value)
+    if (event.target.value == 'male') {
+      this.firstFormGroup.controls.gender.patchValue('male')
+    } else if (event.target.value == 'female') {
+      this.firstFormGroup.controls.gender.patchValue('female')
+    }
+  }
+
+  onSubmit() {
+
+    const formData = new FormData();
+
+    formData.append('fname', this.firstFormGroup.controls.fname.value)
+    formData.append('lname', this.firstFormGroup.controls.lname.value)
+    formData.append('date', this.firstFormGroup.controls.date.value)
+    formData.append('country', this.firstFormGroup.controls.country.value)
+    formData.append('age', this.firstFormGroup.controls.age.value)
+    formData.append('gender', this.firstFormGroup.controls.gender.value)
+    formData.append('terms', this.firstFormGroup.controls.gender.value)
+
+    let object = {};
+    formData.forEach((value, key) => {
+      object[key] = value;
+      if (object[key] == '' || object[key] == undefined) {
+        let element = document.getElementById(key);
+        element.style.border = 'solid red 1px'
+      }
+    })
+    let json = object;
+
+    console.log(this.firstFormGroup)
+    if(this.firstFormGroup.status == 'INVALID') {
+      this.toastrService.error('Please fill all fields and try again.')
+    } else {
+      // Show the new form
+      console.log(document.getElementById('firstFormGroup'))
+      console.log(document.getElementById('secondFormGroup'))
+    }
 
   }
 
